f17cd68852a27022f78f78dbc9838adf5b9891a8
LauraKastenn <laura_kasten@estudante.sesisenai.org.br>
2025-03-18 10:07:51 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//atv 1
+let numero = 1
+function calculo (){
+if (numero % 2 === 0) {
+    console.log ("É um numero par");
+} else {
+    console.log("É um numero impar");
+}
+}
+calculo(numero)
+
+
+//atv 2
+let numeroum = 1
+let numerodois = 2
+function calcular (numeroum, numerodois){
+    console.log (numeroum+numerodois);
+    console.log(numeroum-numerodois);
+    console.log(numeroum*numerodois);
+    console.log(numeroum/numerodois);
+}
+calcular(numeroum, numerodois)
+
+//atv 3
+let count = 10;
+while (count>=1){
+    console.log(count)
+    count--;
+}
+
+
