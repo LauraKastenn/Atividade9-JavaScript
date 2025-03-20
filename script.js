ae5d6289b0e0c110b705a07b20656b0a7d7d3dda
LauraKastenn <laura_kasten@estudante.sesisenai.org.br>
2025-03-20 12:57:29 -0300

    fiz as atividades

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 //atv 1
 let numero = 1
 function calculo (){
@@ -27,5 +28,54 @@ while (count>=1){
     console.log(count)
     count--;
 }
+//atv 4
+let texto = prompt("Digite uma palavra");
+function inverterTexto(texto){
+    console.log(texto.split("").reverse().join(""));
+}
+inverterTexto(texto)
 
+//atv 5
+let palavra = prompt("Digite uma palavra");
+function contarCaracteres(palavra){
+    console.log(palavra.split(''));
+    for (i=0;i<palavra.lenght; i++);
+}
+contarCaracteres(palavra)
+
+//atv 6
+let carro = {
+    nome : prompt("Digite o nome do carro"),
+    modelo : prompt ("Digite o modelo do carro"),
+    ano : prompt("Digite o ano do carro"),
+automovel:function (){
+    console.log(this.modelo);
+}
+};
+carro.automovel();
+
+//atv 7
+let apresentacao = {
+    nome: prompt("Digite um nome"),
+    mensagem: "Ola",
+apresentar:function(){
+    console.log(this.mensagem + " " + this.nome );
+}
+};
+apresentacao.apresentar();
+*/
+//atv 8
+let n1 = prompt ("Digite um numero")*1;
+let n2 = prompt("Digite outro numero")*1;
+let n3 = prompt("Digite outro numero")*1;
+function media(n1, n2, n3){
+    console.log ((n1 + n2 + n3)/3)
+};
+media(n1, n2, n3);
+
+//atv 9 
+let count = 20;
+while (count>=1){
+    if (multiplodetres)
+    console.log ()
 
